b68f4a2894d380af3c242dbf850f299734340793
Jiri {x2} Cincura <jiri@cincura.net>
2015-07-11 12:28:03 +0200

    Line numbers.

--- assets/my.js
+++ assets/my.js
@@ -4,12 +4,27 @@ function initLinks() {
 	$('div.post a:not(a[href*="blog.cincura.net"])').attr('target', '_blank');
 }
 
+function initLineNumbers() {
+	$('div.highlight pre code').each(function(i, e) {
+		e = $(e);
+		var lines = e.html().split(/\n/);
+		var length = lines.length;
+		var spans = '';
+		for (var i = 0; i < length; i++) {
+			if ((i == 0 || i == length - 1) && lines[i] == '')
+				continue;
+			spans += '<span>' + (i + 1) + '</span>';
+		}
+		e.prepend('<span class="line-numbers">' + spans + '</span>');
+	});
+}
+
 function initImageBox() {
 	$('div.post a[href*="blog.cincura.net/i/"]:has(img)').attr('rel', 'gallery').fancybox({
 		openEffect: 'fade',
 		closeEffect: 'fade',
 		nextEffect: 'fade',
-		prevEffect: 'fade'   
+		prevEffect: 'fade'
 	});
 }
 
@@ -28,6 +43,7 @@ function initSearch() {
 
 $(document).ready(function() {
 	initLinks();
+	initLineNumbers();
 	initImageBox();
 	initSearch();
 });
