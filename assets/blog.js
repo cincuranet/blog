80d046ad8d86ff41114b7632c853dbb5d93de93d
Jiri {x2} Cincura <jiri@cincura.net>
2015-07-16 15:14:28 +0200

    Disqus in blog.js

--- assets/blog.js
+++ assets/blog.js
@@ -82,6 +82,23 @@ var blog = (function() {
 		}
 	}
 
+	function initDisqus() {
+		var disqus_shortname = 'blogcincuranet';
+		var disqus_url = '{{ site.disqus_base_url }}{{ page.url }}';
+
+		(function() {
+		    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+		    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+		    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+		(function () {
+		    var s = document.createElement('script'); s.async = true;
+		    s.type = 'text/javascript';
+		    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+		    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+		}());
+	}
+
 	return {
 		initGeneral: function() {
 			initLinks();
@@ -95,6 +112,7 @@ var blog = (function() {
 		},
 		initPostPage: function() {
 			showArticleNicely();
+			initDisqus();
 		}
 	};
 })();
